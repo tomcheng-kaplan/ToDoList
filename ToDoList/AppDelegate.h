a4f46ad9ba0b7f8b11b0233063a60516c25181e6
tomcheng-kaplan <tcheng@395-C02J112XDKQ2.local>
2014-12-22 15:02:54 -0500

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Tom Cheng on 12/22/14.
+//  Copyright (c) 2014 Tom Cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDoList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDoList
+//
+//  Created by Tom Cheng on 12/22/14.
+//  Copyright (c) 2014 Tom Cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
